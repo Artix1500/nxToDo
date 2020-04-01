06571429215753523d04dc7af39576ca6303f093
Artix1500 <artur.haczek23@gmail.com>
2020-04-01 20:15:36 +1100

    another approach

--- libs/todo/data-access/src/lib/+state/todos.effects.ts
+++ libs/todo/data-access/src/lib/+state/todos.effects.ts
@@ -1,31 +1,32 @@
 import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { fetch } from '@nrwl/angular';
-
+import { DataPersistence } from '@nrwl/angular';
 import * as fromTodos from './todos.reducer';
 import * as TodosActions from './todos.actions';
 import { TodosDataService } from '../services/todos-data.service';
 
+import { Effect } from '@ngrx/effects';
+
 @Injectable()
 export class TodosEffects {
-  loadTodos$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(TodosActions.loadTodos),
-      fetch({
-        run: action => {
-          // Your custom service 'load' logic goes here. For now just return a success action...
-          const todos = this.todosDataService.getTodos();
-          return { type: '[Todos] Load Todos Success', payload: todos };
-          return TodosActions.loadTodosSuccess( todos );
-        },
 
-        onError: (action, error) => {
-          console.error('Error', error);
-          return TodosActions.loadTodosFailure({ error });
-        }
-      })
-    )
+  @Effect()
+  loadTodos$ = this.dp.fetch(
+    TodosActions.loadTodos,
+    {
+      run: (action) => {
+        return  TodosActions.loadTodosSuccess(  this.todosDataService.getTodos() );
+      },
+      onError: (action, error) => {
+        console.error('Error', error);
+        return TodosActions.loadTodosFailure({ error });
+      }
+    }
   );
 
-  constructor(private actions$: Actions, private todosDataService: TodosDataService) {}
+  constructor(
+    private dp: DataPersistence<fromTodos.TodosPartialState>,
+    private todosDataService: TodosDataService
+    ) {}
 }
